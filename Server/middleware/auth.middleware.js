27b56c70707b1a5121e38f2d9622b45c009a6665
Fatima1Nasser <s12112649@stu.najah.edu>
2025-11-02 18:47:04 +0200

    story services done almost

---
+++ Server/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js"; 
+
+export  const authMiddleware = {
+
+    async authentication (req , res,next){
+  try {
+   
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // ضعي السر في .env
+
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or expired token" });
+  }
+   }
+};
+
+export default authMiddleware ;
