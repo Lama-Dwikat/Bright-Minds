c8648064dd5c0939b929405a5e19ad10cd60125d
Fatima1Nasser <s12112649@stu.najah.edu>
2025-11-03 00:48:20 +0200

    token for stories done

--- Server/routes/story.route.js
+++ Server/routes/story.route.js
@@ -4,12 +4,12 @@ import authMiddleware from "../middleware/auth.middleware.js";
 export const storyRouter = express.Router();
 
 
-storyRouter.post("story/", authMiddleware.authentication, storyController.createStory);
-storyRouter.put("story/:storyId", authMiddleware.authentication, storyController.updateStory);
-storyRouter.delete("story/:storyId", authMiddleware.authentication, storyController.deleteStory);
-storyRouter.get("story/:storyId", authMiddleware.authentication, storyController.getStoryById);
-storyRouter.get("story/child/:childId", authMiddleware.authentication, storyController.getStoriesByChild);
-storyRouter.post("story/:storyId/media", authMiddleware.authentication, storyController.addMediaToStory);
-storyRouter.post("story/:storyId/submit", authMiddleware.authentication, storyController.submitStory); 
+storyRouter.post("/story/create", authMiddleware.authentication, storyController.createStory);
+storyRouter.put("/story/update/:storyId", authMiddleware.authentication, storyController.updateStory);
+storyRouter.delete("/story/delete/:storyId", authMiddleware.authentication, storyController.deleteStory);
+storyRouter.get("/story/getstorybyid/:storyId", authMiddleware.authentication, storyController.getStoryById);
+storyRouter.get("/story/grtstoriesbychild/:childId", authMiddleware.authentication, storyController.getStoriesByChild);
+storyRouter.post("/story/addmedia/:storyId/media", authMiddleware.authentication, storyController.addMediaToStory);
+storyRouter.post("/story/submit/:storyId/submit", authMiddleware.authentication, storyController.submitStory); 
 
 
