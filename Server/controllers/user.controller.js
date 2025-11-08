c53851dbf4358ae54e40fb2851ee6f2c099c5441
lamadwikat <dwikatlama4@gmail.com>
2025-11-08 17:33:07 +0200

    supervisor page

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -4,19 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const userController = {
 
-    // Create a new user
-    // async createUser(req, res) {
-    //     try {
-    //         const user = await userService.createUser(req.body);
-    //         res.status(201).json(user);
-    //     } catch (error) {
-    //    if (error.message === "Email already exists") {
-    //     return res.status(400).json({ error: error.message });
-    //     }
-    //    res.status(500).json({ error: error.message });
-    //   }
 
-    // },
     async createUser(req, res) {
   try {
     const userData = req.body;
@@ -70,6 +58,7 @@ if (userData.cv) {
           name: user.name,
           email: user.email,
           role: user.role,
+          cvStatus: user.cvStatus
         },
       });
         }
@@ -256,10 +245,34 @@ async linkChildToParent(req, res) {
     }
 },
 
+    async updateCvStatus(req, res) {
+        try {
+            const { status } = req.body;
+            const user = await userService.updateCvStatus(req.params.id, status);
+            res.status(200).json(user);
+        } catch (error) {
+            if(error.message==="CV status can only be updated for supervisors"){
+                return res.status(400).json({error: error.message});
+            }
+            res.status(500).json({ error: error.message });
+        }
+    },
 
+    async addAGeGroupToSupervisor(req, res) {
+        try {
+            const {ageGroup}=req.body;
+            const user =await userService.addAGeGroupToSupervisor(req.params.id, ageGroup);
+            res.status(200).json(user); 
+        }
+       catch(error){
+        if(error.message==="Only supervisors can add age groups" || error.message==="User not found"){
+            return res.status(400).json({error: error.message});
+        }
+        res.status(500).json({ error: error.message });
 
+    }
 
-
+    },
 
 
 }
